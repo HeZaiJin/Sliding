0a5189a92a847f47d913fe4a21f5fc11e4c2b15d
HeZaiJin <1376277145@qq.com>
2017-02-26 17:00:02 +0800

    support scrollView( but intercept has bug)

--- app/src/main/java/com/haozhang/demo/MainActivity.java
+++ app/src/main/java/com/haozhang/demo/MainActivity.java
@@ -2,9 +2,6 @@ package com.haozhang.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.content).setOnTouchListener(new View.OnTouchListener() {
+        /*findViewById(R.id.content).setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 Log.d("SlidingLayout-Activity", "onTouch() called with: event = [" + event + "]");
                 return true;
             }
-        });
+        });*/
     }
 }
--- sliding/src/main/java/com/haozhang/sliding/SlidingLayout.java
+++ sliding/src/main/java/com/haozhang/sliding/SlidingLayout.java
@@ -2,23 +2,26 @@ package com.haozhang.sliding;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
 import android.widget.Scroller;
 
 /**
  * @author HaoZhang
  * @date 2017/2/23.
  */
-public class SlidingLayout extends ViewGroup {
+public class SlidingLayout extends FrameLayout {
     private static final String TAG = "SlidingLayout";
 
-    private static final int DEFULT_SLIDING_MIN_DISTANCE = 10;
+    private static final int DEFAULT_SLIDING_MIN_DISTANCE = 10;
+    private static final float DEFAULT_DECAY_VALUE = 0.5f;
     private int mSlidingMaxDistance;
     private Scroller mScroller;
 
@@ -60,7 +63,7 @@ public class SlidingLayout extends ViewGroup {
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                smoothScrollTo(0,0);
+                smoothScrollTo(0, 0);
                 break;
         }
         mLastY = y;
@@ -70,18 +73,23 @@ public class SlidingLayout extends ViewGroup {
     private void smoothScrollTo(int fx, int fy) {
         int dx = fx - mScroller.getFinalX();
         int dy = fy - mScroller.getFinalY();
-
-        smoothScrollBy(dx, dy);
+        mScroller.startScroll(mScroller.getFinalX(), mScroller.getFinalY(), dx, dy);
+        invalidate();
     }
 
     private void smoothScrollBy(int dx, int dy) {
-        mScroller.startScroll(mScroller.getFinalX(), mScroller.getFinalY(), dx, dy);
+        int finalY = mScroller.getFinalY();
+        int absY = Math.abs(finalY);
+        if (absY > mSlidingMaxDistance) {
+            dy *= DEFAULT_DECAY_VALUE;
+        }
+        mScroller.startScroll(mScroller.getFinalX(), finalY, dx, dy);
         invalidate();
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        Log.d(TAG, "onInterceptTouchEvent() called with: event = " + event);
+        Log.d(TAG, "onInterceptTouchEvent() called with: event = [" + event + "]");
         boolean intercept = false;
         // 记录此次触摸事件的y坐标
         float y = event.getY();
@@ -93,21 +101,13 @@ public class SlidingLayout extends ViewGroup {
             }
             case MotionEvent.ACTION_MOVE: {
                 event.getRawY();
-                if (y > mLastY) { // 下滑操作
-                    View child = getChildAt(0);
-                    if (child instanceof AdapterView) {
-                        AdapterView adapterChild = (AdapterView) child;
-                        // 判断AbsListView是否已经到达内容最顶部(如果已经到达最顶部，就拦截事件，自己处理滑动)
-                        if (adapterChild.getFirstVisiblePosition() == 0
-                                || adapterChild.getChildAt(0).getTop() == 0) {
-                            intercept = true;
-                        }
-                    }
-                    float getX = child.getX();
-                    float translationX = child.getTranslationX();
-                    int scrollX = child.getScrollX();
-                    Log.d(TAG, "getX :" + getX + ", translationX :" + translationX + ", scrollX :" + scrollX);
-                    intercept = true;
+                View child = getChildAt(0);
+                if (y > mLastY) {
+                    // 下滑操作
+                    intercept = interceptPullDown(child);
+                } else {
+                    // 上滑动
+                    intercept = interceptPullUp(child);
                 }
                 break;
             }
@@ -120,6 +120,95 @@ public class SlidingLayout extends ViewGroup {
         return intercept;
     }
 
+    private boolean interceptPullDown(View child) {
+        boolean intercept = true;
+        if (child instanceof ScrollView) {
+            intercept = interceptPullDownScrollView(child);
+        } else if (child instanceof AdapterView) {
+            intercept = interceptPullDownAdapterView(child);
+        } else if (child instanceof RecyclerView) {
+            intercept = interceptPullDownRecyclerView(child);
+        }
+        return intercept;
+    }
+
+    private boolean interceptPullUp(View child) {
+        boolean intercept = true;
+        if (child instanceof ScrollView) {
+            intercept = interceptPullUpScrollView(child);
+        } else if (child instanceof AdapterView) {
+            intercept = interceptPullUpAdapterView(child);
+        } else if (child instanceof RecyclerView) {
+            intercept = interceptPullUpRecyclerView(child);
+        }
+        return intercept;
+    }
+
+    private boolean interceptPullDownAdapterView(View child) {
+        boolean intercept = true;
+        AdapterView adapterChild = (AdapterView) child;
+        // 判断AbsListView是否已经到达内容最顶部
+        if (adapterChild.getFirstVisiblePosition() != 0
+                || adapterChild.getChildAt(0).getTop() != 0) {
+            // 如果没有达到最顶端，则仍然将事件下放
+            intercept = false;
+        }
+        return intercept;
+    }
+
+    private boolean interceptPullUpAdapterView(View child) {
+        boolean intercept = false;
+        AdapterView adapterChild = (AdapterView) child;
+
+        // 判断AbsListView是否已经到达内容最底部
+        if (adapterChild.getLastVisiblePosition() == adapterChild.getCount() - 1
+                && (adapterChild.getChildAt(adapterChild.getChildCount() - 1).getBottom() == getMeasuredHeight())) {
+            // 如果到达底部，则拦截事件
+            intercept = true;
+        }
+        return intercept;
+    }
+
+    private boolean interceptPullDownScrollView(View child) {
+        boolean intercept = false;
+        if (child.getScrollY() <= 0) {
+            intercept = true;
+        }
+        return intercept;
+    }
+
+    private boolean interceptPullUpScrollView(View child) {
+        boolean intercept = false;
+        ScrollView scrollView = (ScrollView) child;
+        View scrollChild = scrollView.getChildAt(0);
+
+        if (scrollView.getScrollY() >= (scrollChild.getHeight() - scrollView.getHeight())) {
+            intercept = true;
+        }
+        return intercept;
+    }
+
+    private boolean interceptPullDownRecyclerView(View child) {
+        boolean intercept = false;
+        RecyclerView recyclerChild = (RecyclerView) child;
+        if (recyclerChild.computeVerticalScrollOffset() <= 0)
+            intercept = true;
+
+        return intercept;
+    }
+
+    private boolean interceptPullUpRecyclerView(View child) {
+        boolean intercept = false;
+
+        RecyclerView recyclerChild = (RecyclerView) child;
+        if (recyclerChild.computeVerticalScrollExtent() + recyclerChild.computeVerticalScrollOffset()
+                >= recyclerChild.computeVerticalScrollRange())
+            intercept = true;
+
+        return intercept;
+    }
+
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         return super.dispatchTouchEvent(event);
@@ -127,98 +216,44 @@ public class SlidingLayout extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        final int size = getChildCount();
-        final int parentWidthSize = MeasureSpec.getSize(widthMeasureSpec);
-        final int paretnHeightSize = MeasureSpec.getSize(heightMeasureSpec);
-        for (int i = 0; i < size; ++i) {
-            final View child = getChildAt(i);
-            if (child.getVisibility() != GONE) {
-                LayoutParams childLp = child.getLayoutParams();
-                final boolean childWidthWC = childLp.width == LayoutParams.WRAP_CONTENT;
-                final boolean childHeightWC = childLp.height == LayoutParams.WRAP_CONTENT;
-                int childWidthMeasureSpec;
-                int childHeightMeasureSpec;
-                if (child.getLayoutParams() instanceof MarginLayoutParams) {
-                    MarginLayoutParams childMarginLp = (MarginLayoutParams) childLp;
-                    childWidthMeasureSpec = childWidthWC ? MeasureSpec.makeMeasureSpec(parentWidthSize, MeasureSpec.UNSPECIFIED)
-                            : getChildMeasureSpec(widthMeasureSpec,
-                            getPaddingLeft() + getPaddingRight() + childMarginLp.leftMargin + childMarginLp.rightMargin, childLp.width);
-                    childHeightMeasureSpec = childHeightWC ? MeasureSpec
-                            .makeMeasureSpec(paretnHeightSize,
-                                    MeasureSpec.UNSPECIFIED)
-                            : getChildMeasureSpec(heightMeasureSpec,
-                            getPaddingTop() + getPaddingBottom()
-                                    + childMarginLp.topMargin
-                                    + childMarginLp.bottomMargin,
-                            childMarginLp.height);
-                } else {
-                    childWidthMeasureSpec = childWidthWC ? MeasureSpec
-                            .makeMeasureSpec(parentWidthSize,
-                                    MeasureSpec.UNSPECIFIED)
-                            : getChildMeasureSpec(widthMeasureSpec,
-                            getPaddingLeft() + getPaddingRight(),
-                            childLp.width);
-                    childHeightMeasureSpec = childHeightWC ? MeasureSpec
-                            .makeMeasureSpec(paretnHeightSize,
-                                    MeasureSpec.UNSPECIFIED)
-                            : getChildMeasureSpec(heightMeasureSpec,
-                            getPaddingTop() + getPaddingBottom(),
-                            childLp.height);
-                }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (getChildCount() > 0) {
+            final View child = getChildAt(0);
+            int height = getMeasuredHeight();
+            if (child.getMeasuredHeight() < height) {
+                final FrameLayout.LayoutParams lp = (LayoutParams) child.getLayoutParams();
+
+                int childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec,
+                        getPaddingLeft() + getPaddingRight(), lp.width);
+                height -= getPaddingTop();
+                height -= getPaddingBottom();
+                int childHeightMeasureSpec =
+                        MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
                 child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
             }
         }
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
     }
-
-    float mTotalLength;
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int childStartPostion = 0;
-        mTotalLength = 0;
-        final int count = getChildCount();
-        if (count == 0) {
-            return;
+        super.onLayout(changed, l, t, r, b);
+        final int childHeight = (getChildCount() > 0) ? getChildAt(0).getMeasuredHeight() : 0;
+        final int scrollRange = Math.max(0,
+                childHeight - (b - t - getPaddingBottom() - getPaddingBottom()));
+        int mScrollY = 0;
+        // Don't forget to clamp
+        if (mScrollY > scrollRange) {
+            mScrollY = scrollRange;
+        } else if (mScrollY < 0) {
+            mScrollY = 0;
         }
-        childStartPostion = getPaddingTop();
-        for (int i = 0; i < count; i++) {
-            final View child = getChildAt(i);
-            if (child != null && child.getVisibility() != View.GONE) {
-                LayoutParams lp = child.getLayoutParams();
-                final int childHeight = child.getMeasuredHeight();
-                int leftMargin = 0;
-                int rightMargin = 0;
-                int topMargin = 0;
-                int bottomMargin = 0;
-                if (lp instanceof MarginLayoutParams) {
-                    MarginLayoutParams mlp = (MarginLayoutParams) lp;
-                    leftMargin = mlp.leftMargin;
-                    rightMargin = mlp.rightMargin;
-                    topMargin = mlp.topMargin;
-                    bottomMargin = mlp.bottomMargin;
-                }
-
-                childStartPostion += topMargin;
-                int startX = (getWidth() - leftMargin - rightMargin - child
-                        .getMeasuredWidth()) / 2 + leftMargin;
-                child.layout(startX, childStartPostion,
-                        startX + child.getMeasuredWidth(), childStartPostion
-                                + childHeight);
-                childStartPostion += (childHeight + bottomMargin);
-            }
-        }
-        childStartPostion += getPaddingBottom();
-        mTotalLength = childStartPostion;
+        scrollTo(0, mScrollY);
     }
 
     @Override
     public void computeScroll() {
         if (mScroller.computeScrollOffset()) {
-            System.out.println("computeScroll()---> " +
-                    "mScroller.getCurrX()=" + mScroller.getCurrX() + "," +
-                    "mScroller.getCurrY()=" + mScroller.getCurrY());
             scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
             postInvalidate();
         }
